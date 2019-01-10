cb4c6777a5626bdb41aea7938ff9b7f257e786d6
Frank Wong <forstudy0218@gmail.com>
2019-01-10 21:45:23 +0000

    Add searchbox choice and reset after search

--- static/scripts.js
+++ static/scripts.js
@@ -4,11 +4,41 @@ $(document).ready(function(){
 });
 function init(){
     renew();
+    navsearch();
 }
 // https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing_clock
 function renew() {
     let date = new Date();
     $("#localtime").html(date);
-    let t = setTimeout(renew, 500)
+    let t = setTimeout(renew, 500);
 }
-$("#gsearchbox").submit(); // https://stackoverflow.com/questions/7704976
+// https://stackoverflow.com/questions/10011802/
+function navsearch() {
+    $('#navselect').change(function() {
+        let site = $('option:selected',this).attr('value');
+        if (site === "search") {
+            $('#navsearchbox').attr("action", "https://www.google.com/search");
+            $('#navquery').attr("name", "q");
+            $('#navquery').attr("placeholder", "Google it!");
+        }
+        if (site === "map") {
+            $('#navsearchbox').attr("action", "https://www.google.com/maps");
+            $('#navquery').attr("name", "q");
+            $('#navquery').attr("placeholder", "Where to go?");
+        }
+        if (site === "youtube") {
+            $('#navsearchbox').attr("action", "https://www.youtube.com/results");
+            $('#navquery').attr("name", "search_query");
+            $('#navquery').attr("placeholder", "Watch this!");
+        }
+    });
+}
+// Default after submit
+function navsubmit () {
+    setTimeout(function() {
+        $('#navsearchbox').attr("action", "https://www.google.com/search");
+        $('#navquery').attr("name", "q");
+        $('#navquery').attr("placeholder", "Google it!");
+        $('#navsearchbox')[0].reset();
+    }, 500);
+}
