bd7352d7bc73ad52a47839ca19cdc00b3bfa4834
Frank Wong <forstudy0218@gmail.com>
2019-01-10 19:34:39 +0000

    Change the navbar and form behavior. Add simple google search

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 TODO
 
 1. capture screen from page
-2. search box with opening new page
+2. Sass maybe?
+3. search box with opening new page
--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,14 @@
 // A $( document ).ready() block.
+$(document).ready(function(){
+    init();
+});
+function init(){
+    renew();
+}
 // https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing_clock
-$(document).ready(function renew() {
+function renew() {
     let date = new Date();
     $("#localtime").html(date);
     let t = setTimeout(renew, 500)
-});
+}
+$("#gsearchbox").submit(); // https://stackoverflow.com/questions/7704976
