95cb56c145a41e1aa42844a5c2569f46ba5defef
Frank Wong <forstudy0218@gmail.com>
2019-02-04 15:01:56 +0000

    Change on ww-en: add zh-TW. Change on google search box, add lr.

--- static/enwolfjs.js
+++ static/enwolfjs.js
@@ -1,6 +1,6 @@
 // global variables
 let gbv = {
-    debug: false,
+    debug: true,
     state: {
         players: [], // players
         // game state
@@ -13,6 +13,7 @@ let gbv = {
             nightcount: 0,
             lasthang: "",
         },
+        langset: "en",
     },
     setPlayers (newList) {
         if (this.debug) console.log('players changed to', newList);
@@ -51,6 +52,11 @@ let gbv = {
         localStorage.setItem("enwwstate", JSON.stringify(this.state.gamestate));
         changebody();
         controlcentre.resetData();
+    },
+    setLang (boolzh) {
+        if (this.debug) console.log("Chinese", boolzh);
+        this.state.langset = (boolzh) ? "zh" : "en";
+        localStorage.setItem("enwwlang", this.state.langset);
     }
 };
 // inital setting (load save)
@@ -73,6 +79,11 @@ if (!localStorage.getItem("enwwstate")) {
 } else {
     gbv.state.gamestate = JSON.parse(localStorage.getItem("enwwstate"));
     changebody();
+}
+if (!localStorage.getItem("enwwdata")) {
+    localStorage.setItem("enwwlang", "en");
+} else {
+    gbv.state.langset = localStorage.getItem("enwwlang");
 }
 
 // body color
@@ -242,6 +253,11 @@ var popmodal = new Vue ({
             this.body = body;
             this.footer = footer;
             this.headerColor = headcolor;
+            if (gbv.state.langset === "zh") {
+                this.header = (dictionary.modalmess[this.header] || header);
+                this.body = (dictionary.modalmess[this.body] || body);
+                this.footer = (dictionary.modalmess[this.footer] || footer);
+            }
             this.showModal = true;
         }
     }
@@ -284,7 +300,8 @@ var playercreation = new Vue ({
     data: {
         shared: gbv,
         addquery: "",
-        delquery: ""
+        delquery: "",
+        langzh: false,
     },
     methods: {
         addPlayer: function() {
@@ -292,7 +309,7 @@ var playercreation = new Vue ({
             for (let i = 0; i < gbv.state.players.length; i++) {
                 if (newname === gbv.state.players[i]["playname"]) {
                     this.addquery = "";
-                    popmodal.showmessage("Warning", "Name already exist.", "Try another one.", "Red");
+                    popmodal.showmessage("WARNING", "Name already exist.", "Try another one.", "Red");
                     return;
                 }
             }
@@ -317,6 +334,7 @@ var playercreation = new Vue ({
                 return;
             } else {
                 gbv.setGamestate ("phase", 1);
+                this.shared.setLang (this.langzh);
             }
         }
     }
@@ -328,13 +346,13 @@ var gamesetting = new Vue({
     data: {
         shared: gbv,
         roleset: [
-            {name: "Villager", selid: 0, short: "ve", faction: "Good"},
-            {name: "Fortuneteller", selid: 0, short: "ft", faction: "Good"},
-            {name: "Hunter", selid: 0, short: "ht", faction: "Good"},
-            {name: "Medium", selid: 0, short: "md", faction: "Good"},
-            {name: "Werewolf", selid: 0, short: "ww", faction: "Evil"},
-            {name: "Madman", selid: 0, short: "mm", faction: "Evil"},
-            {name: "Spirit-Fox", selid: 0, short: "sf", faction: "Spirit-Fox"}
+            {name: "Villager", selid: 0, short: "ve", faction: "Good", zhnm:"村民"},
+            {name: "Fortuneteller", selid: 0, short: "ft", faction: "Good", zhnm:"占卜師"},
+            {name: "Hunter", selid: 0, short: "ht", faction: "Good", zhnm:"獵人"},
+            {name: "Medium", selid: 0, short: "md", faction: "Good", zhnm:"靈媒"},
+            {name: "Werewolf", selid: 0, short: "ww", faction: "Evil", zhnm:"狼人"},
+            {name: "Madman", selid: 0, short: "mm", faction: "Evil", zhnm:"狂人"},
+            {name: "Spirit-Fox", selid: 0, short: "sf", faction: "Spirit-Fox", zhnm:"妖狐"}
         ],
         styleSetNum: {},
         voteset: "",
@@ -389,7 +407,8 @@ var gamesetting = new Vue({
                     background: "yellow",
                     color: "black"
                 };
-                return "Too Many Role";
+                let too_many = (this.askLang) ? "...過多身分" : "...Too Many Role";
+                return too_many;
             } else if (selectedcount < playerscount) {
                 this.styleSetNum = {
                     border: "thick solid yellow",
@@ -405,6 +424,9 @@ var gamesetting = new Vue({
                 };
                 return 0;
             }
+        },
+        askLang: function() {
+            return (this.shared.state.langset === "zh");
         }
     }
 });
@@ -521,46 +543,69 @@ var controlcentre = new Vue({
             let ttlist = this.shared.state.players.filter(el => el.alive === 1);
             // ft, ht, md, ww have action, sf, mm, ve have none
             if (rolestr === "ft") {
-                this.firstnotice = "Check one player each night.";
-                this.rolenotice = "Choose one to see whether he/she is Werewolf. Spirit-Fox would die if chosen.";
+                if (this.askLang) {
+                    this.firstnotice = "占卜師每晚能查看一名玩家";
+                    this.rolenotice = "請選一名對象,查看對方是否狼人; 如對象為妖狐,對象將會死亡";
+                } else {
+                    this.firstnotice = "Check one player each night.";
+                    this.rolenotice = "Choose one to see whether he/she is Werewolf. Spirit-Fox would die if chosen.";
+                }
                 ttlist = ttlist.filter(el => el.playname !== this.playername);
             } else if (rolestr === "ht") {
-                this.firstnotice = "Protect one from Werewolf attack";
-                if (this.shared.state.gamestate.nightcount === 0) {
-                    this.rolenotice = "Werewolf won't attack this night. Choose anyone.";
+                let firststr;
+                let otherstr;
+                if (this.askLang) {
+                    this.firstnotice = "獵人每晚能保護一名玩家,免被狼人殺死";
+                    firststr = "狼人第一晚不會攻擊, 請隨意選一人:";
+                    otherstr = "請選擇你想要保護的人:";
                 } else {
-                    this.rolenotice =  "Choose one to protect.";
+                    this.firstnotice = "Protect one from Werewolf attack";
+                    firststr = "Werewolf won't attack this night. Choose anyone.";
+                    otherstr = "Choose one to protect.";
                 }
-
+                if (this.shared.state.gamestate.nightcount === 0) {
+                    this.rolenotice = firststr;
+                } else {
+                    this.rolenotice = otherstr;
+                }
             } else if (rolestr === "md") {
                 let hangname = this.shared.state.gamestate.lasthang;
                 if (this.shared.state.gamestate.nightcount !== 0) {
                     if (hangname !== "") {
                         let hangedone = this.shared.state.players.filter(el => el.playname === hangname);
                         let hangedrole = hangedone[0]["role"];
+                        let pre_sen = (this.askLang) ? "白天被驅逐的" : "Last exile ";
                         if (hangedrole === "ww") {
-                            this.firstnotice = "Last exile " + hangname + " was Werewolf!";
+                            let suf_yes = (this.askLang) ? "是狼人!" : " was Werewolf!";
+                            this.firstnotice = pre_sen + hangname + suf_yes;
                         } else {
-                            this.firstnotice = "Last exile " + hangname + " was NOT Werewolf";
+                            let suf_no = (this.askLang) ? "是無辜的 (不是狼人)" : " was NOT Werewolf";
+                            this.firstnotice = pre_sen + hangname + suf_no;
                         }
                     } else {
-                        this.firstnotice = "No exile today.";
+                        this.firstnotice = (this.askLang) ? "白天沒有人被驅逐" : "No exile today.";
                     }
                 } else {
-                    this.firstnotice = "No exile on the first night";
+                    this.firstnotice = (this.askLang) ? "第一天不會有人被驅逐" : "No exile on the first night";
                 }
-                this.rolenotice = "Choose anyone.";
+                this.rolenotice = (this.askLang) ? "請隨意選一人:" : "Choose anyone.";
             } else if (rolestr === "ww") {
-                this.firstnotice = "Werewolf player(s): ";
+                this.firstnotice = (this.askLang) ? "以下玩家是狼人:" : "Werewolf player(s): ";
                 if (this.shared.state.gamestate.nightcount === 0) {
-                    this.rolenotice = "Can't attack on the first night. Choose anyone.";
+                    let first_nt;
+                    if (this.askLang) {
+                        first_nt ="第一晚不能攻擊, 請隨意選一人:";
+                    } else {
+                        first_nt = "Can't attack on the first night. Choose anyone.";
+                    }
+                    this.rolenotice = first_nt;
                 } else {
-                    this.rolenotice = "Choose the one you want to attack.";
+                    this.rolenotice = (this.askLang) ? "請選一人作為襲擊對象:" : "Choose the one you want to attack.";
                     ttlist = ttlist.filter(el => el.role !== "ww");
                 }
             } else {
-                this.firstnotice = "You do nothing at night.";
-                this.rolenotice =  "Choose anyone.";
+                this.firstnotice = (this.askLang) ? "晚上你沒有能力" : "You do nothing at night.";
+                this.rolenotice =  (this.askLang) ? "請隨意選一人:" : "Choose anyone.";
             }
             for (let i = 0; i < ttlist.length; i++) {
                 this.targetList.push(ttlist[i]["playname"]);
@@ -574,33 +619,44 @@ var controlcentre = new Vue({
             if (rolestr === "ft") {
                 let ttlist = this.shared.state.players.filter(el => el.playname === ttnm);
                 let ttrole = ttlist[0]["role"];
+                let ft_no = (this.askLang) ? "是無辜的 (不是狼人)" : " is NOT Werewolf!";
                 if (ttrole === "ww") {
-                    bodymessage = ttnm + " is Werewolf!";
+                    let ft_yes = (this.askLang) ? "是狼人!" : " is Werewolf!";
+                    bodymessage = ttnm + ft_yes;
                     popmodal.showmessage("Result", bodymessage, "Tell everyone during daytime!", "Red");
                 } else if (ttrole === "sf") {
                     this.pushSave("nightdead", ttnm);
-                    bodymessage = ttnm + " is NOT Werewolf.";
+                    bodymessage = ttnm + ft_no;
                     popmodal.showmessage("Result", bodymessage, "Target would die if role is Spirit-Fox.", "Green");
                 } else {
-                    bodymessage = ttnm + " is NOT Werewolf.";
+                    bodymessage = ttnm + ft_no;
                     popmodal.showmessage("Result", bodymessage, "Target would die if role is Spirit-Fox.", "Green");
                 }
             } else if (rolestr === "ht") {
                 if (this.shared.state.gamestate.nightcount === 0) {
                     popmodal.showmessage("Sleeping", "Waiting for next day.", "ZzZzzZ...", "Green");
                 } else {
-                     this.pushSave("protectlist", ttnm);
-                    bodymessage = ttnm + " would be protected.";
-                    popmodal.showmessage("Done", bodymessage, "Werewolf can't kill him/her now.", "Green");
+                    this.pushSave("protectlist", ttnm);
+                    let this_pro = (this.askLang) ? "將會被保護" : " would be protected.";
+                    bodymessage = ttnm + this_pro;
+                    popmodal.showmessage("Done", bodymessage, "Werewolf can't kill him/her tonight.", "Green");
                 }
             } else if (rolestr === "ww") {
                 if (this.shared.state.gamestate.nightcount === 0) {
                     popmodal.showmessage("Sleeping", "Waiting for next day.", "ZzZzzZ...", "Green");
                 } else {
                     this.pushSave("nightww", ttnm);
-                    let wwtitle = "Your target is " + ttnm + ".";
-                    let footermessage = "Wish the target is not protected by Hunter or Spirit-Fox.";
-                    bodymessage = "If other Werewolf alive choose another target, your target might not be attacked this night.";
+                    let wwtitle;
+                    wwtitle = (this.askLang) ? "你的目標是" + ttnm : "Your target is " + ttnm + ".";
+                    let footermessage;
+                    let bodymessage;
+                    if (this.askLang) {
+                        footermessage = "如對象為妖狐; 或被獵人保護的玩家 ,襲擊會失敗";
+                        bodymessage = "若其他生存的狼人選了其他目標, 你的目標或許不會被襲擊";
+                    } else {
+                        footermessage = "Wish the target is not protected by Hunter or Spirit-Fox.";
+                        bodymessage = "If other Werewolf alive choose another target, your target might not be attacked tonight.";
+                    }
                     popmodal.showmessage(wwtitle, bodymessage, footermessage, "Green");
                 }
             } else {
@@ -633,7 +689,7 @@ var controlcentre = new Vue({
                 }
             }
             bodymessage = bodymessage.slice(0, -2);
-            bodymessage += " became dead.";
+            bodymessage += (this.askLang) ? "已經死亡" : " became dead.";
             if (this.saved.nightdead.length === 0) {
                 // no death alert
                 popmodal.showmessage("Night End.", "No one died this night.", "Game keep going...", "Green");
@@ -687,9 +743,13 @@ var controlcentre = new Vue({
             let newhang = "";
             this.changeSave("exilecolor", "Red");
             if (this.saved.vtexile === "") {
-                this.changeSave("vtexile", "NO EXILE");
+                if (this.askLang) {
+                    this.changeSave("vtexile", "無");
+                } else {
+                    this.changeSave("vtexile", "NO EXILE");
+                }
                 this.changeSave("exilecolor", "black");
-                popmodal.showmessage("Result", "No one gone this time.", "Good luck next time.", "Green");
+                popmodal.showmessage("Vote Result", "No one gone this time.", "Good luck next time.", "Green");
             } else {
                 for (let j = 0; j < this.shared.state.players.length; j++) {
                     if (this.saved.vtexile === this.shared.state.players[j]["playname"]) {
@@ -733,7 +793,7 @@ var controlcentre = new Vue({
             let rolelist = gamesetting.roleset;
             for (let j = 0; j < rolelist.length; j++) {
                 if (rolelist[j]["short"] === this.playrole) {
-                    rolenm = rolelist[j]["name"];
+                    rolenm = (this.askLang) ? rolelist[j]["zhnm"] : rolelist[j]["name"];
                     break;
                 }
             }
@@ -796,6 +856,9 @@ var controlcentre = new Vue({
         },
         playstageCheck: function() {
             return this.shared.state.gamestate.playstage;
+        },
+        askLang: function() {
+            return (this.shared.state.langset === "zh");
         }
     },
     mounted() {
@@ -828,7 +891,7 @@ var gamresult = new Vue ({
                 for (let j = 0; j < rolelist.length; j++) {
                     if (rolelist[j]["short"] === roleshort) {
                         factionstr = rolelist[j]["faction"] + factionstr;
-                        rolenm = rolelist[j]["name"];
+                        rolenm = (this.askLang) ? rolelist[j]["zhnm"] : rolelist[j]["name"];
                         break;
                     }
                 }
@@ -840,19 +903,23 @@ var gamresult = new Vue ({
     },
     computed: {
         getTitle: function () {
+            let win_text;
             if (this.shared.state.gamestate.winside === 1) {
+                win_text = (this.askLang) ? "善良陣營勝利" : "Good Faction Win";
                 return {
-                    text: "Good Faction Win",
+                    text: win_text,
                     color: "green",
                 };
             } else if (this.shared.state.gamestate.winside === 2) {
+                win_text = (this.askLang) ? "邪惡陣營勝利" : "Evil Faction Win";
                 return {
-                    text: "Evil Faction Win",
+                    text: win_text,
                     color: "red",
                 };
             } else if (this.shared.state.gamestate.winside === 3) {
+                win_text = (this.askLang) ? "妖狐陣營勝利" : "Spirit-Fox Faction Win";
                 return {
-                    text: "Spirit-Fox Faction Win",
+                    text: win_text,
                     color: "purple",
                 };
             } else {
@@ -872,5 +939,68 @@ var gamresult = new Vue ({
             let dies = this.shared.state.players.filter(el => el.alive === 0);
             return this.calList(dies);
         },
+        askLang: function() {
+            return (this.shared.state.langset === "zh");
+        }
     },
 });
+
+// translate
+const dictionary = {
+    modalmess: {
+        "Spirit-Fox Faction Win!": "妖狐陣營勝利",
+        "Good Job!": "村子已被妖狐吞噬",
+        "Pause and restart.": "請按暫停來重開始",
+        "Evil Faction Win!": "邪惡陣營勝利",
+        "Appreciate!": "全村已被狼人吃光",
+        "Good Faction Win!": "善良陣營勝利",
+        "Congratulations!": "一切終於回歸平靜",
+        "Notification": "提醒",
+        "Game paused.": "暫停中",
+        "Game restarted.": "已重新開始",
+        "WARNING": "警告",
+        "At least five player.": "至少五名玩家",
+        "Invite more friends to play.": "請邀請更多人",
+        "Name already exist.": "重複名稱",
+        "Try another one.": "試試新一個",
+        "Done": "完成",
+        "Randomizing.": "隨機分配中",
+        "Vote setting is empty.": "請選擇投票選項",
+        "Choose one.": "選一個吧",
+        "At least one werewolf.": "最少一名狼人",
+        "Add one werewolf.": "你懂遊戲怎麼玩嗎",
+        "Counts of players and roles are not match.": "人數不符合",
+        "Assign roles again.": "注意職業數量",
+        "Is this you?": "這是你嗎?",
+        "If no, leave the device.": "不是的話,請不要繼續操作",
+        "You voted.": "你已投票",
+        "Below is who you voted.": "按鈕旁寫的是你所投的對象",
+        "Confirmed": "已確認",
+        "If stage not end, next player please.": "請交給還未確認的人",
+        "All players confirmed": "所有人都已確認",
+        "First night arriving...": "第一晚來臨...",
+        "Tell everyone during daytime!": "請活到白天再說出來",
+        "Result": "占卜結果",
+        "Target would die if role is Spirit-Fox.": "若對象是妖狐, 對方將會死亡",
+        "Werewolf can't kill him/her tonight.": "這晚狼人將不能殺死你選擇的對象",
+        "Sleeping": "發個好夢",
+        "Waiting for next day.": "靜候明天",
+        "Night End.": "夜晚結束",
+        "No one died this night.": "這晚沒人死亡",
+        "Game keep going...": "遊戲繼續...",
+        "If Werewolfs do not choose the same target, only one of the most chosen target(s) would be attacked.":
+        "狼人若選了不同目標, 最終目標為最多狼人選擇的其中一人",
+        "Rule for Werewolf attack": "狼人襲擊規則",
+        "Spirit-Fox would survive this attack.": "無論如何, 妖狐不會被你殺死",
+        "Time to leave.": "是時候離開了",
+        "But game may not be over though...": "但不代表就此結束",
+        "Vote Result": "投票結果",
+        "No one gone this time.": "這次無人離開",
+        "Good luck next time.": "下次好運",
+        "Night Coming": "黑夜將至",
+        "Everyone be ready.": "做好準備",
+        "Be more careful...": "需要更小心了",
+        "Your Role": "你的職業",
+        "Don't forget it.": "請牢記",
+    },
+};
