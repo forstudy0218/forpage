33da06870f1c6511e8c77014c2a76168331d3d40
Frank Wong <forstudy0218@gmail.com>
2019-04-29 17:27:21 +0000

    change on map: add map3

--- static/dorumap.js
+++ static/dorumap.js
@@ -5,6 +5,7 @@ var dorumap = new Vue ({
         mapLinks: [
             "https://www.google.com/maps/d/embed?mid=1mpwTdMyfeIxW-hPIuxNJWBvEhmbLpO8T",
             "https://www.google.com/maps/d/embed?mid=1tepL3mWWTHbmlP1MNfgGVr8v6MerxQRo",
+            "https://www.google.com/maps/d/u/0/embed?mid=1nDmqeP0z1dj88erO_c9fXtVoSymXjHXW",
         ],
         mapsrc: "",
     },
