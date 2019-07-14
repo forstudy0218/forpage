0572d7003b713df9f39d1d6a668a36a5fb45a962
forstudy0218 <forstudy0218@gmail.com>
2019-07-14 12:38:45 +0000

    change on ONW: prototype for night

--- static/one_night_ww.js
+++ static/one_night_ww.js
@@ -17,6 +17,10 @@ const messages = {
       title: 'Paused',
       removal: 'New Game',
     },
+    errorType: {
+      noName: 'Name(s) Missing',
+      dupName: 'Same Name existed',
+    },
     roles: {
       // "ww", "ft", "tf", "ve"
       ww: "Werewolf",
@@ -28,6 +32,29 @@ const messages = {
       rolesAre: "Roles in this game: ",
       placeholder: "Your Name",
       startBtn: 'Start',
+    },
+    console: {
+      night: 'Night Time. Confirm your role. Use ability if any.',
+      day: 'Day Time. Discuss. And then vote.',
+      askForPlayer: 'Press the above button with your name to progress.',
+      askIfPlayer: 'Are You ',
+      IAm: 'I am ',
+      showMyRole: 'Show My Role',
+      stageEnd: 'All players finished their action. Press the button below.',
+      nextStage: 'Next',
+      YouAre: 'You are ',
+      st_ww: "You can see the name of all Werewolf player(s).",
+      nd_ww: "Choose any player. Nothing will happen.",
+      rd_ww: "Press confirm to see who's Werewolf!",
+      st_ft: "You can see the role of one player, or all the role(s) that not in the game.",
+      nd_ft: "Choose one of players to see, or choose 'not in game'.",
+      rd_ft: "Press confirm to see the role(s)!",
+      st_tf: "You may exchange role with one player. You will know what role you become.",
+      nd_tf: "Choose your own name if don't want to exchange. Else, choose your target's name.",
+      rd_tf: "Press confirm to exchange. Your target won't know if the role is exchanged!",
+      st_ve: "You do nothing at night.",
+      nd_ve: "Choose any player. Nothing will happen.",
+      rd_ve: "Press confirm.",
     },
   },
   "zh-Hant":{
@@ -47,6 +74,10 @@ const messages = {
       title: '暫停中',
       removal: '結束遊戲',
     },
+    errorType: {
+      noName: '玩家必須擁有名字',
+      dupName: '玩家名稱不能重覆',
+    },
     roles: {
       // "ww", "ft", "tf", "ve"
       ww: "狼人",
@@ -58,6 +89,29 @@ const messages = {
       rolesAre: "這次的角色： ",
       placeholder: "輸入名字",
       startBtn: '開始遊戲',
+    },
+    console: {
+      night: '到晚上了<br>確認玩家身份<br>發動應有的能力。',
+      day: '到早上了<br>討論大家的身份<br>準備好就投票。',
+      askForPlayer: '請按上方的玩家名字繼續遊戲',
+      askIfPlayer: '你是否 ',
+      IAm: '我是 ',
+      showMyRole: '決定我的角色',
+      stageEnd: '所有玩家都完成行動，請按下繼續',
+      nextStage: '繼續',
+      YouAre: '你是 ',
+      st_ww: "你一會可以看到所有狼人的名字.",
+      nd_ww: "但你什麼都做不了，請隨便選一位玩家名字",
+      rd_ww: "請按下確認，你將會看到狼人的名字",
+      st_ft: "你查看一名玩家的身份，或查看所有未被分配的身份",
+      nd_ft: "選擇目標的名字，或，選擇「未分配」",
+      rd_ft: "請按下確認，你將會看到對像的身份",
+      st_tf: "你可與一名玩家交換身份，而對方是不會知道身份被交換的",
+      nd_tf: "選擇目標的名字。如果不想交換，請選擇自己的名字",
+      rd_tf: "請按下確認，將會顯示你交換後的角色",
+      st_ve: "你沒有特殊能力",
+      nd_ve: "所以你什麼都不能做，請隨便選一位玩家名字",
+      rd_ve: "請按下確認",
     },
   },
   ja: {
@@ -73,6 +127,10 @@ const messages = {
       returnGame: '再開',
       title: '一時停止中',
       removal: 'ゲームをやめる',
+    },
+    errorType: {
+      noName: '名前がいない',
+      dupName: '名前の重複を避ける',
     },
     roles: {
       // "ww", "ft", "tf", "ve"
@@ -115,6 +173,7 @@ function load_save() {
 
 // https://stackoverflow.com/questions/2450954/
 // randomize list
+// use testshiffle.py to check probi
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -132,6 +191,12 @@ function shuffle(array) {
   }
 
   return array;
+}
+
+// check unique primitive array
+// https://stackoverflow.com/questions/9229645/
+function isUnique(array) {
+  return array.every((item, index, self) => self.indexOf(item) === index);
 }
 
 // Create a Vue instance with `i18n` option
@@ -158,8 +223,20 @@ new Vue({
     // role list
     roles: ["ww", "ww", "ft", "tf", "ve", "ve", "ve", "ve"],
     playerNames: [],
+    // use for render name button
+    is_checking: false,
+    // use for render console
+    player_toggle: false,
+    stage_end: false,
+    show_role: false,
+    night_result: false,
+    user_name: "",
+    user_role: "",
     // use for pause
     temp_stage: 0,
+    // use for error handle
+    toggle_error: false,
+    err_type: "",
   },
   methods: {
     // on lang change
@@ -188,6 +265,15 @@ new Vue({
     // to naming page from front page
     goToName: function() {
       const amount_selected = this.players_amount;
+      // check playerNames
+      while (this.playerNames.length !== amount_selected) {
+        // this.playerNames.length ( < )||( > ) amount_selected
+        if (this.playerNames.length > amount_selected) {
+          this.playerNames.splice(amount_selected);
+        } else {
+          this.playerNames.push("");
+        }
+      }
       this.roles.splice(amount_selected + 2);
       const temp_arr = [...this.roles];
       shuffle(temp_arr);
@@ -201,13 +287,98 @@ new Vue({
       }
       this.presisted.stage = 1;
       // for check
-      if (this.debug) console.log(this.presisted);
+      if (this.debug) console.log(this.playerNames);
     },
     // start the game
     gameStart: function() {
-      if (this.debug) console.log('game starting');
+      if (this.debug) console.log(this.playerNames);
+      // Check if name exist
+      if (this.playerNames.includes("")) {
+        this.err_type = "noName";
+        this.toggle_error = true;
+        return;
+      }
+      // check if item unique
+      if (!isUnique(this.playerNames)) {
+        this.err_type = "dupName";
+        this.toggle_error = true;
+        return;
+      }
+      // copy presisted.players
+      const obj_to_save = JSON.parse(JSON.stringify(this.presisted.players));
+      for (let nm = 0; nm < this.playerNames.length; nm++) {
+        obj_to_save[nm].name = this.playerNames[nm];
+      }
+      // saving
+      this.savePlayers(obj_to_save);
       this.saveRemain(this.presisted.remain);
       this.saveStage(2);
+      this.toggle_error = false;
+    },
+    // valid name button
+    toggleNameButton: function(namestr) {
+      let thisdata = this.presisted.players.filter(el => el.name === namestr)[0];
+      let stage = this.presisted.stage;
+      return ((thisdata.night && stage === 2) || (this.is_checking) || (thisdata.voted && stage === 3));
+    },
+    // change name button color
+    getCls: function(namestr) {
+      let thisdata = this.presisted.players.filter(el => el.name === namestr)[0];
+      if ( (this.presisted.stage === 2 && thisdata.night) || (thisdata.voted && stage === 3) ) {
+          return "btn btn-success";
+      } else {
+      return "btn btn-warning";
+      }
+    },
+    // handle player selected
+    toggleInfo: function(namestr) {
+      let playerdata = this.presisted.players.filter(el => el.name === namestr)[0];
+      this.user_name = playerdata.name;
+      this.user_role = playerdata.origin;
+      this.player_toggle = true;
+    },
+    // handle player confirm self
+    playerConfirmeSelf: function() {
+      this.is_checking = true;
+    },
+    // handle player want to see
+    roleShow: function() {
+      this.show_role = true;
+    },
+    // handle role ability
+    // big function incoming
+    useAbility: function(roleStr) {
+      this.night_result = true;
+      this.show_role = false;
+    },
+    // handle player night result
+    endPlayerConsole: function(namestr) {
+      // check stage
+      let stageStr;
+      if (this.presisted.stage === 2 ) stageStr = "night";
+      if (this.presisted.stage === 3 ) stageStr = "voted";
+      // copy presisted.players
+      const obj_to_save = JSON.parse(JSON.stringify(this.presisted.players));
+      const thisdata = obj_to_save.filter(el => el.name === namestr)[0];
+      thisdata[stageStr] = true;
+      this.savePlayers(obj_to_save);
+      this.checkStageEnd(this.presisted.stage);
+      this.night_result = false;
+      this.is_checking = false;
+      this.player_toggle = false;
+    },
+    // check if all checked
+    checkStageEnd: function(stageNum) {
+      if (this.debug) console.log("Checking if stage end", stageNum);
+      let stageStr;
+      if (stageNum !== 2 && stageNum !== 3) return;
+      if (stageNum === 2 ) stageStr = "night";
+      if (stageNum === 3 ) stageStr = "voted";
+      if ( this.presisted.players.every(el => el[stageStr]) ) {
+        if (this.debug) console.log("should end");
+        this.stage_end = true;
+      }
+      if (this.debug) console.log(this.stage_end);
     },
     // pause fuction
     togglePause: function() {
@@ -233,6 +404,7 @@ new Vue({
       };
       this.role = ["ww", "ww", "ft", "tf", "ve", "ve", "ve", "ve"];
       localStorage.removeItem('onwdata');
+      this.toggle_error = false;
     },
     // toggle debug
     debugMode: function(){
@@ -251,13 +423,14 @@ new Vue({
       if (this.debug) console.log("Save found.");
       // adapt
       try {
-        this.presisted = payload
+        this.presisted = payload;
       } catch(e) {
         // can't map save to data
         localStorage.removeItem('onwdata');
         load_save();
       }
-    };
+      this.checkStageEnd(this.presisted.stage);
+    }
     if (this.debug) console.log("Successfully Mounted.");
   },
 }).$mount('#app');
