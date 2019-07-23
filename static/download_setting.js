441401fd842a99f0db4bd02fef0557ad035e02c5
forstudy0218 <forstudy0218@gmail.com>
2019-07-23 12:44:15 +0000

    add onw.apk And change onw button And update dl page

--- static/download_setting.js
+++ static/download_setting.js
@@ -3,11 +3,16 @@ var download_page = new Vue ({
     data: {
         apkLinks: [
            "static/assets/sdk3.apk",
+           "static/assets/onw.apk",
         ],
         info: [
             {
-                app_name: "Sudoku",
-                file_name: "sudoku0218 (android 5 up)",
+                app_name: "Sudoku (android 5 or up)",
+                file_name: "sudoku0218",
+            },
+            {
+                app_name: "One Night Werewolf (android 5 or up)",
+                file_name: "onw0218",
             },
         ],
         color_name: ["white", "#968c8c"],
