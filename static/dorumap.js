80d3441d83d4b4aba6a14ac799ca5fd1efbd7ce7
forstudy0218 <forstudy0218@gmail.com>
2019-07-31 16:48:23 +0000

    map: 5th

--- static/dorumap.js
+++ static/dorumap.js
@@ -7,6 +7,7 @@ var dorumap = new Vue ({
             "https://www.google.com/maps/d/embed?mid=1tepL3mWWTHbmlP1MNfgGVr8v6MerxQRo",
             "https://www.google.com/maps/d/u/0/embed?mid=1nDmqeP0z1dj88erO_c9fXtVoSymXjHXW",
             "https://www.google.com/maps/d/u/0/embed?mid=1xkzhWPq8YRmHaYp_pQ0hbx_pTrwx4gWR",
+            "https://www.google.com/maps/d/embed?mid=12xv4jP4h8iRRRfkB8oPlU-qSbpDunv_b",
         ],
         mapsrc: "",
     },
