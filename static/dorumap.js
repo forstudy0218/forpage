6ac5c5b67838602001ea2fe101e550fcf0583b9a
forstudy0218 <forstudy0218@gmail.com>
2020-06-06 05:35:37 +0000

    change on map: map13

--- static/dorumap.js
+++ static/dorumap.js
@@ -15,6 +15,7 @@ var dorumap = new Vue ({
             "https://www.google.com/maps/d/embed?mid=1a1sekNasjOmmEqwLVtz9Tk0DV3PVnRBC",
             "https://www.google.com/maps/d/embed?mid=1BGtC3VRFGXBIJA1GGH-XoMaKgklJDdpZ",
             "https://www.google.com/maps/d/embed?mid=1S9cHQB5X6Hp5SUy4Wmiyxr0UeoZTQmbZ",
+            "https://www.google.com/maps/d/embed?mid=1qd8Hslc3tCMGRTqpXSM2ZoXajLxZqFZp",
         ],
         mapsrc: "",
     },
