bff75498f603fd2983a1a8c97d6ba964b91dafc9
forstudy0218 <forstudy0218@gmail.com>
2020-11-29 17:19:36 +0000

    change on map: add28

--- static/dorumap.js
+++ static/dorumap.js
@@ -30,6 +30,7 @@ var dorumap = new Vue ({
             "https://www.google.com/maps/d/embed?mid=1oeywwIFLjSGBttSrxfSVUCuV7R3DUx7s",
             "https://www.google.com/maps/d/embed?mid=1w5x8BvhXFQ1ZtVhCzh9m7QVNVf6B4FEU",
             "https://www.google.com/maps/d/embed?mid=1y_b-Ave3Lrp84tLDiYEAgZ1VS7vIxp8b",
+            "https://www.google.com/maps/d/embed?mid=1gh5XLzu1bY9CNppeX4lPICtRwGN0lIZZ",
         ],
         mapsrc: "",
     },
